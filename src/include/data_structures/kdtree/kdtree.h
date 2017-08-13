9e8f8ba379df58b2bb5657d97be0e53d84dc5061
HarishKarunakaran <karunaha73925@gmail.com>
2017-08-13 05:47:49 +0530

    Added Functions to insert, delete, find min and max from KdTree

--- src/include/data_structures/kdtree/kdtree.h
+++ src/include/data_structures/kdtree/kdtree.h
@@ -1,3 +1,10 @@
+/*
+ * This code is developed to implement the KdTree data structure
+ * The authors of this code are Harish Karunakaran, Shravan Murlidaran
+ * The code is available at https://github.com/HarishKarunakaran/Data_Structures/
+ *
+ */
+
 #ifndef KDTREE_H
 #define KDTREE_H
 
@@ -10,15 +17,68 @@
 template <typename T>
 class KdTree
 {
-    std::vector<std::vector<T> > data_;
-    int children_;
+    /**
+         * \brief Root Node of the KdTree
+         */
+    std::shared_ptr<Node<T> > data_;
+    /**
+         * \brief splitting axis of the data
+         */
+    int cutDimension_;
+    /**
+         * \brief dimension of the input data
+         */
+    int dataDimension_;
+
+protected:
+    /**
+         * \brief insert Node into KdTree
+         */
+    bool insert(std::vector<T>& , std::shared_ptr<Node<T> >, int);
+    /**
+         * \brief delete node from KdTree
+         */
+    std::vector<T>& delete_node(std::vector<T>& , std::shared_ptr<Node<T> >, int);
+    /**
+         * \brief Find Minimum along a specified axis
+         */
+    std::vector<T>& min(std::shared_ptr<Node<T> >, int, int);
+    /**
+         * \brief Find Maximum along a specified axis
+         */
+    std::vector<T>& max(std::shared_ptr<Node<T> >, int, int);
+
+
 public:
-    KdTree(std::vector<std::vector<T> >& , int children = 2);
+    /**
+         * \brief Initiates a KdTree object
+         */
+    KdTree(int cutDimension_ = 0);
+    /**
+         * \brief Delete KdTree Object
+         */
     ~KdTree();
+    /**
+         * \brief Create Tree structure for given input data
+         */
+    bool buildTree(std::vector<std::vector<T> >& );
+    /**
+         * \brief Helper function to insert node
+         */
     bool insertNode(std::vector<T>& );
+    /**
+         * \brief Helper function to delete node
+         */
     bool deleteNode(std::vector<T>& );
-    void findMin(std::vector<T>& , int axis);
-    void findMax(std::vector<T>& , int axis);
+    /**
+         * \brief Helper function to find minimum along a specified axis
+         */
+    void findMin(std::vector<T>&, int);
+    /**
+         * \brief Helper function to find maximum along a specified axis
+         */
+    void findMax(std::vector<T>&, int);
+
 };
 
 #endif // KDTREE_H
--- src/kdtree/kdtree.cpp
+++ src/kdtree/kdtree.cpp
@@ -1,12 +1,9 @@
 #include <data_structures/kdtree/kdtree.h>
 
 template <typename T>
-KdTree<T>::KdTree(std::vector<std::vector<T> >& data, int children) : children_(children), data_(data)
+KdTree<T>::KdTree(int cd) : cutDimension_(cd)
 {
-    if(!data_.empty())
-    {
 
-    }
 }
 
 template <typename T>
@@ -14,3 +11,111 @@ KdTree<T>::~KdTree()
 {
 
 }
+
+template <typename T>
+bool KdTree<T>::insertNode(std::vector<T>& data)
+{
+    if(data_.empty()) { data_->value_ = data; dataDimension_ = data.size();}//shd look up
+    return insert(data, data_, cutDimension_);
+}
+
+template <typename T>
+bool KdTree<T>::insert(std::vector<T>& data, std::shared_ptr<Node<T> > kd_node, int cd)
+{
+    if(kd_node == nullptr)  kd_node = std::make_shared<Node<T> >(data);
+
+    else if(data == kd_node->value_)   return false;
+
+    else if(data[cd] < kd_node->value_[cd]) kd_node->nodes_[0] = insert(data, kd_node->nodes_[0], (cd+1) % dataDimension_);
+
+    else    kd_node->nodes_[1] = insert(data, kd_node->nodes_[1], (cd+1) % dataDimension_);
+
+    return true;
+}
+
+
+template <typename T>
+bool KdTree<T>::buildTree(std::vector<std::vector<T> >& data)
+{
+    for(const auto& node_data : data)
+        insertNode(node_data);
+}
+
+template <typename T>
+void KdTree<T>::findMin(std::vector<T>& minimum, int axis)
+{
+    if(data_.empty()) return;
+     minimum = min(data_, axis, cutDimension_);
+}
+
+template <typename T>
+std::vector<T>& KdTree<T>::min(std::shared_ptr<Node<T> > kd_node, int axis, int cd)
+{
+    if (kd_node == nullptr) return;
+    // T splits on the dimension we’re searching
+    // => only visit left subtree
+    if (cd == axis)
+    if (kd_node->nodes_[0] == nullptr) return kd_node->value_;
+
+    else return min(kd_node->nodes_[0], axis, (cd+1) % dataDimension_);
+    // T splits on a different dimension
+    // => have to search both subtrees
+    else
+        //shd look up minimum
+        return std::min({min(kd_node->nodes_[0], axis, (cd + 1) % dataDimension_), min(kd_node->nodes_[0], axis, (cd + 1) % dataDimension_), kd_node->value_});
+}
+
+template <typename T>
+void KdTree<T>::findMax(std::vector<T>& maximum, int axis)
+{
+    if(data_.empty()) return;
+    maximum = max(data_, axis, cutDimension_);
+}
+
+template <typename T>
+std::vector<T>& KdTree<T>::max(std::shared_ptr<Node<T> > kd_node, int axis, int cd)
+{
+    if (kd_node == nullptr) return;
+    // T splits on the dimension we’re searching
+    // => only visit left subtree
+    if (cd == axis)
+    if (kd_node->nodes_[0] == nullptr) return kd_node->value_;
+
+    else return max(kd_node->nodes_[0], axis, (cd+1) % dataDimension_); //shd change
+    // T splits on a different dimension
+    // => have to search both subtrees
+    else
+        //shd look up maximum
+        return std::max({max(kd_node->nodes_[0], axis, (cd + 1) % dataDimension_), max(kd_node->nodes_[0], axis, (cd + 1) % dataDimension_), kd_node->value_[axis]});
+}
+
+//template <typename T>
+//bool KdTree<T>::insertNode(std::vector<T>& data, int cd)
+//{
+//    if(data_.empty()) data_->value_ = data;
+
+//    std::shared_ptr<Node<T> > temp_node = std::make_shared<Node<T> >(data_);
+
+//    while(true)
+//    {
+//        if(data == temp_node->value_)    return false;
+
+//        else if(data[cd] < temp_node->value_[cd])
+//        {
+//            if(temp_node->nodes_[0] == nullptr) {temp_node->nodes_[0] = std::make_shared<Node<T> >(data); return true;}
+//            temp_node = temp_node->nodes_[0];
+//            cd = (cd + 1) % data.size();
+//        }
+
+//        else
+//        {
+//            if(temp_node->nodes_[1] == nullptr) {temp_node->nodes_[1] = std::make_shared<Node<T> >(data); return true;}
+//            temp_node = temp_node->nodes_[1];
+//            cd = (cd + 1) % data.size();
+//        }
+
+
+//    }
+
+//}
+
