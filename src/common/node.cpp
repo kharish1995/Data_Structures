b6895ff637303a1a379cbe9fd53ac377d29c85e3
HarishKarunakaran <karunaha73925@gmail.com>
2017-08-27 03:33:01 +0530

    Add error checking for vectors

--- src/common/node.cpp
+++ src/common/node.cpp
@@ -3,9 +3,9 @@
 template <typename T>
 void Node<T>::setNode(const std::shared_ptr<Node<T> >& node, u_int8_t i)
 {
-    if (i > nodes_.size())
-        std::cerr << "Index out of bounds for the node" << std::endl;
-    nodes_[i] = node;
+    if (i <= nodes_.size())
+        nodes_[i] = node;
+    std::cerr << "Index out of bounds for the node" << std::endl;
 }
 
 template <typename T>
@@ -29,9 +29,9 @@ Node<T>::Node(std::vector<T>* value, int size) : nodes_(size, nullptr)
 template <typename T>
 T Node<T>::getValue(u_int8_t i) const
 {
-    if (i > value_.size())
-        std::cerr << "Index out of bounds for the node" << std::endl;
-    return value_[i];
+    if (i <= value_.size())
+        return value_[i];
+    std::cerr << "Index out of bounds for the node" << std::endl;
 }
 
 template <typename T>
@@ -43,12 +43,13 @@ std::vector<T> Node<T>::getValues() const
 template <typename T>
 std::shared_ptr<Node<T> > Node<T>::getNode(u_int8_t i) const
 {
-    if (i > nodes_.size())
-        std::cerr << "Index out of bounds for the node" << std::endl;
-    return nodes_[i];
+    if (i <= nodes_.size())
+        return nodes_[i];
+    std::cerr << "Index out of bounds for the node" << std::endl;
 }
 
 template <typename T>
 Node<T>::~Node()
 {
+
 }
